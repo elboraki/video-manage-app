b48097eea2bc7d06c87b671e81a86734a3cf8c7d
elboraki <younes.elboraki@gmail.com>
2024-06-23 00:47:47 +0100

    connect server to mongodb server

--- vp-bnd/routes/api.js
+++ vp-bnd/routes/api.js
@@ -1,6 +1,20 @@
 const expres = require('express')
 const router = expres.Router()
+const mongoose=require('mongoose')
 
+const db='mongodb://localhost:27017/videos'
+mongoose.Promise=global.Promise
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database', error);
+  });
 
 router.get('/', (req, res) => {
     res.status(200).send('Server is working')
