95c9e4da0d4dd3e8b42cb97f5bc12935362062ce
elboraki <younes.elboraki@gmail.com>
2024-06-23 01:09:31 +0100

    Implemented get videos list and video by id

--- vp-bnd/routes/api.js
+++ vp-bnd/routes/api.js
@@ -1,23 +1,39 @@
 const expres = require('express')
 const router = expres.Router()
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
+const Video = require('../model/video')
 
-const db='mongodb://localhost:27017/videos'
-mongoose.Promise=global.Promise
+const db = 'mongodb://localhost:27017/videos'
+mongoose.Promise = global.Promise
 
 mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Successfully connected to the database');
-  })
-  .catch((error) => {
-    console.error('Error connecting to the database', error);
-  });
+})
+    .then(() => {
+        console.log('Successfully connected to the database');
+    })
+    .catch((error) => {
+        console.error('Error connecting to the database', error);
+    });
 
-router.get('/', (req, res) => {
-    res.status(200).send('Server is working')
+router.get('/videos', async (req, res) => {
+    try {
+        const videos = await Video.find({});
+        res.json(videos);
+    } catch (err) {
+        console.log("Error getting videos", err);
+        res.status(500).send({ error: 'Error getting videos' });
+    }
+})
+
+router.get('/videos/:id', async (req, res) => {
+    try {
+        const videos = await Video.findById(req.params.id);
+        res.json(videos);
+    } catch (err) {
+        res.status(500).send({ error: 'Error getting video by ID' });
+    }
 })
 
 
