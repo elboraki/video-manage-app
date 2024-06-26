4916e7bb736332488cb89c8e5af5afa1d225d72e
elboraki <younes.elboraki@gmail.com>
2024-06-27 00:31:48 +0100

    added redoc for api and env config

--- vp-bnd/server.js
+++ vp-bnd/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const redoc = require('redoc-express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const dotenv=require('dotenv').config()
 
 const api = require('./routes/api')
 
@@ -15,7 +16,47 @@ app.use(express.static(path.join(__dirname,'/dist')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+if(process.env.NODE_ENV==='developement'){
+
+    app.get('/docs/swagger.json', (req, res) => {
+        res.sendFile('swagger.json', { root: '.' });
+      });
+    
+      app.get(
+        '/docs',
+        redoc({
+          title: 'API Docs',
+          specUrl: '/docs/swagger.json',
+          nonce: '', // <= it is optional,we can omit this key and value
+          // we are now start supporting the redocOptions object
+          // you can omit the options object if you don't need it
+          // https://redocly.com/docs/api-reference-docs/configuration/functionality/
+          redocOptions: {
+            theme: {
+              colors: {
+                primary: {
+                  main: '#6EC5AB'
+                }
+              },
+              typography: {
+                fontFamily: `"museo-sans", 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+                fontSize: '15px',
+                lineHeight: '1.5',
+                code: {
+                  code: '#87E8C7',
+                  backgroundColor: '#4D4D4E'
+                }
+              },
+              menu: {
+                backgroundColor: '#ffffff'
+              }
+            }
+          }
+        })
+      );
+      app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
+
 
 app.use('/api',api)
 
@@ -26,5 +67,5 @@ app.get('*', (req, res) => {
 
 
 app.listen(port,() => {
-    console.log("Yns");
+    console.log("PPT",process.env);
 })
