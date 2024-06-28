db7470f298feb8240d1044b30c119772a9c63e60
elboraki <younes.elboraki@gmail.com>
2024-06-28 01:01:50 +0100

    installed primeng css framework

--- vp-fr/src/app/components/video-list/video-list.component.ts
+++ vp-fr/src/app/components/video-list/video-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-video-list',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './video-list.component.html',
   styleUrl: './video-list.component.scss'
 })
